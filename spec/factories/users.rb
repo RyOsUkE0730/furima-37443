9998a2b3fc1ffea8151b87244fa41929d60ae526
RyOsUkE <ryosukek74@gmail.com>
2022-02-10 16:12:06 +0900

    Preparing for the test
    ・rspec/FactoryBot/Fakerの導入
    ・UsersモデルのFactoryBotを指定

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    first_name {'名'}
+    last_name {'性'}
+    first_name_kana {'メイ'}
+    last_name_kana {'セイ'}
+    birthday {'2000-01-01'}
+  end
+end
