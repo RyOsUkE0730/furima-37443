5a26cf005cd9b39f224eb94088575d5e15129838
RyOsUkE <ryosukek74@gmail.com>
2022-02-11 16:56:20 +0900

    Setting validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :item_name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :prefecture_id
+    validates :delivery_time_id
+    validates :selling_price
+  end
 end
