fcf5cddefc7802b202bb528ff0f37940c2e50f93
RyOsUkE <ryosukek74@gmail.com>
2022-02-11 18:26:50 +0900

    Create ActiveHash#condition

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :item_name
@@ -17,6 +18,6 @@ class Item < ApplicationRecord
 
   with_options numericality: { other_than: 0, message: "can't be blank" } do
     validates :category_id
-
+    validates :condition_id
   end 
 end
