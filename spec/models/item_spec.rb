721baf72dd0323fa9d7f34a59d4e62f583485c94
RyOsUkE <ryosukek74@gmail.com>
2022-02-14 19:58:49 +0900

    テストコードを修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,13 +32,13 @@ RSpec.describe Item, type: :model do
       end
 
       it 'カテゴリーが空では登録できない' do
-        @item.category_id = ''
+        @item.category_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
       it '商品状態が空では登録できない' do
-        @item.condition_id = ''
+        @item.condition_id = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
@@ -62,7 +62,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '商品価格が空では登録できない' do
-        @item.selling_price = ''
+        @item.selling_price = '0'
         @item.valid?
         expect(@item.errors.full_messages).to include('Selling price is not included in the list')
       end
