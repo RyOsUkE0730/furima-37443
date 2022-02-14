8e90be17177d93a7b94134f4bc7f826be3939051
RyOsUkE <ryosukek74@gmail.com>
2022-02-14 19:58:29 +0900

    migrateファイルを更新
    ・default: 0を削除
    ・外部キーの修正

--- db/migrate/20220211040142_create_items.rb
+++ db/migrate/20220211040142_create_items.rb
@@ -3,13 +3,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
       t.string :item_name, null: false
       t.text :description, null: false
-      t.integer :category_id, default: 0, null: false
-      t.integer :condition_id, default: 0, null: false
-      t.integer :delivery_charge_id, default: 0, null: false
-      t.integer :prefecture_id, default: 0, null: false
-      t.integer :delivery_time_id, default: 0, null: false
+      t.integer :category_id, null: false
+      t.integer :condition_id, null: false
+      t.integer :delivery_charge_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :delivery_time_id, null: false
       t.integer :selling_price, null: false
-      t.references :user, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
