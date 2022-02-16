e89a9e5cc107508b1346c133006886a7c6fec0fd
RyOsUkE <ryosukek74@gmail.com>
2022-02-16 22:46:55 +0900

    provisional)) Create orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+  def index
+    @order = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:item_id, :order_id)
+  end
 end
