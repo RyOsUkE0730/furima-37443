9c5836424043374afca30e643ccdd65ef09a68e1
RyOsUkE <ryosukek74@gmail.com>
2022-02-16 22:47:21 +0900

    Update buyer model+

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,18 +1,19 @@
 class Buyer < ApplicationRecord
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :telephone_num
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_num
   belongs_to :order
 
   with_options presence: true do
-    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id
+    validates :post_code, format: { with:  }
+    validates :prefecture_id numericality: { other_than: 0, message: "can't be blank" }
     validates :municipalities
     validates :address
-    validates :telephone_num,format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :telephone_num,format: { with:  }
   end
 
+
   def save
-    Order.create( item_id: item_id, order_id: order_id )
-    Buyer.create( post_code: post_code, preprefecture_id: prefecture_id, municipalities: municipalities, address: address, telephone_num: telephone_num )
+    order = Order.create( item_id: item_id, order_id: order_id )
+    Buyer.create( post_code: post_code, preprefecture_id: prefecture_id, municipalities: municipalities, building_name: building_name, address: address, telephone_num: telephone_num, order_id: order.id )
   end
 end
