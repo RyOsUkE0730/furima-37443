c85eb5d5766acc5b11bafa2df74127f92f20f432
RyOsUkE <ryosukek74@gmail.com>
2022-02-16 17:33:38 +0900

    Setting validation/buyer

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,3 +1,11 @@
 class Buyer < ApplicationRecord
   belongs_to : order
+
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id
+    validates :municipalities 
+    validates :address
+    validates :telephone_num
+  end
 end
