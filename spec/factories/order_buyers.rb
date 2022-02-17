99fe5da69eb19332c38262e7f028451301f523f4
RyOsUkE <ryosukek74@gmail.com>
2022-02-17 16:36:37 +0900

    Create test code
    ・購入画面実装時点でのvalidationテスト実施

---
+++ spec/factories/order_buyers.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_buyer do
+    post_code { '000-0000' }
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    municipalities { Faker::Address.city }
+    address { Faker::Address.street_name }
+    telephone_num { Faker::Number.number(digits: 10) }
+    building_name { Faker::Address.street_name }
+  end
+end
