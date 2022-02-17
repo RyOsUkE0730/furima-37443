017fde9f557a708c64f8e213f14dae43a2fa442b
RyOsUkE <ryosukek74@gmail.com>
2022-02-17 15:24:00 +0900

    Update order_buyer.rb

--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,6 +1,6 @@
 class OrderBuyer
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_num
+  attr_accessor :post_code, :prefecture_id, :municipalities, :address, :building_name, :telephone_num, :item_id, :user_id
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ }
@@ -11,8 +11,8 @@ class OrderBuyer
   end
 
   def save
-    order = Order.create(item_id: item_id, order_id: order_id)
-    Buyer.create(post_code: post_code, preprefecture_id: prefecture_id, municipalities: municipalities,
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Buyer.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities,
                  building_name: building_name, address: address, telephone_num: telephone_num, order_id: order.id)
   end
 end
